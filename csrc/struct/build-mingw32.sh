3da25b2bb3b189d59a7f2cc2af7294bd0b94e572
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-01-23 00:52:55 +0200

    removed CFLAGS/CXXFLAGS

--- csrc/struct/build-mingw32.sh
+++ csrc/struct/build-mingw32.sh
@@ -1,4 +1,4 @@
-gcc $CFLAGS struct.c -shared -o ../../bin/mingw32/clib/struct.dll -I. -I../lua -ansi -L../../bin/mingw32 -llua51 \
+gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/mingw32/clib/struct.dll -I. -I../lua -ansi -L../../bin/mingw32 -llua51 \
 	-DSTRUCT_INT="long long" \
 	-Wall \
 	-W -pedantic \
