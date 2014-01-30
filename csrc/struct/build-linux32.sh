17b2727b9cbccd6420be6c617447adb8f4df3b37
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-01-30 19:32:14 +0000

    linux32 build

--- csrc/struct/build-linux32.sh
+++ csrc/struct/build-linux32.sh
@@ -1,1 +1,2 @@
-gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/linux32/clib/struct.so -I. -I../lua -ansi -Wall -pedantic
+gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/linux32/clib/struct.so -I. -I../lua -ansi -Wall -pedantic \
+    -DSTRUCT_INT="long long" -Wno-long-long
--- csrc/struct/build-mingw32.sh
+++ csrc/struct/build-mingw32.sh
@@ -1,2 +1,2 @@
 gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/mingw32/clib/struct.dll -I. -I../lua -L../../bin/mingw32 -llua51 -ansi -Wall -pedantic
-	-DSTRUCT_INT="long long"
+	-DSTRUCT_INT="long long" -Wno-long-long
