1a7cf44654d5fdc1bede07190a79e466d1072d0b
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-01-30 16:37:01 +0000

    linux64 build

--- csrc/struct/build-linux32.sh
+++ csrc/struct/build-linux32.sh
@@ -1,13 +1,1 @@
-gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/linux32/clib/struct.so -I. -I../lua -ansi \
-	-DSTRUCT_INT="long long" \
-	-Wall \
-	-W -pedantic \
-	-Waggregate-return \
-	-Wcast-align \
-	-Wmissing-prototypes \
-	-Wnested-externs \
-	-Wpointer-arith \
-	-Wshadow \
-	-Wwrite-strings
-
-#../../linux/bin/luajit teststruct.lua
+gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/linux32/clib/struct.so -I. -I../lua -ansi -Wall -pedantic
--- csrc/struct/build-mingw32.sh
+++ csrc/struct/build-mingw32.sh
@@ -1,13 +1,2 @@
-gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/mingw32/clib/struct.dll -I. -I../lua -ansi -L../../bin/mingw32 -llua51 \
-	-DSTRUCT_INT="long long" \
-	-Wall \
-	-W -pedantic \
-	-Waggregate-return \
-	-Wcast-align \
-	-Wmissing-prototypes \
-	-Wnested-externs \
-	-Wpointer-arith \
-	-Wshadow \
-	-Wwrite-strings
-
-#../../bin/luajit teststruct.lua
+gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/mingw32/clib/struct.dll -I. -I../lua -L../../bin/mingw32 -llua51 -ansi -Wall -pedantic
+	-DSTRUCT_INT="long long"
