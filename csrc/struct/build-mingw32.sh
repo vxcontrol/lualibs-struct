0b0394c7c3506bcae7f5c1276cad4dbd93c74b0c
Cosmin Apreuetsei <cosmin.apreutesei@gmail.com>
2014-02-01 00:53:01 +0200

    bugfix in build script

--- csrc/struct/build-mingw32.sh
+++ csrc/struct/build-mingw32.sh
@@ -1,2 +1,2 @@
-gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/mingw32/clib/struct.dll -I. -I../lua -L../../bin/mingw32 -llua51 -ansi -Wall -pedantic
-	-DSTRUCT_INT="long long" -Wno-long-long
+gcc -O2 -s -static-libgcc struct.c -shared -o ../../bin/mingw32/clib/struct.dll -I. -I../lua -L../../bin/mingw32 -llua51 \
+	-DSTRUCT_INT="long long" -Wno-long-long -ansi -Wall -pedantic
